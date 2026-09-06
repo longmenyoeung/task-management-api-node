fc4642d6daf02c02ab950c2da4cad7b222caaf83
longmenyoeung <longmenyoeung@gmail.com>
2026-09-06 23:17:13 +0700

    model task

--- src/models/TaskModel.js
+++ src/models/TaskModel.js
@@ -1,3 +1,45 @@
+import mongoose from "mongoose";
 
+const taskSchema = new mongoose.Schema({
+    title: {
+        type:String,
+        trim:true,
+        require:true
+    },
+    description: {
+        type:String,
+        trim:true
+    },
+    status: {
+        type:String,
+        enum:['pending', 'dilivered', 'approved'],
+        default: 'pending'
+    },
+    priority: {
+        type:String,
+        trim: true,
+        require: true
+    },
+    project: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'Project',
+        index: true,
+        required: true
+    },
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        index: true,
+        required: true
+    },
+    createAt :{
+        type: Date,
+        default : Date.now
+    }
+},{
+    timestamps : true,
+    collection: 'tasks'
+});
 
-
+const TaskModel = mongoose.model('Task', taskSchema);
+export default TaskModel;

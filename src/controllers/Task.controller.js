0c8725232c3ff04f1dd2649b9f4f4f319dbc0d9f
longmenyoeung <longmenyoeung@gmail.com>
2026-09-07 23:45:25 +0700

    create task

--- src/controllers/Task.controller.js
+++ src/controllers/Task.controller.js
@@ -1,4 +1,7 @@
+import mongoose from "mongoose";
+import ProjectModel from "../models/ProjectModel.js";
 import TaskModel from "../models/TaskModel.js";
+import UserModel from "../models/UserModel.js";
 
 export const getListTask = async (req,res) => {
     try {
@@ -16,6 +19,51 @@ export const getListTask = async (req,res) => {
         return res.status(500).json({
             success: false,
             message: 'Server internal error.',
+            error: error.message
+        });
+    }
+}
+
+export const createTask  = async (req, res) => {
+    try {
+        const {title, description, priority, assignedTo} = req.body;
+        const {id} = req.params;
+
+        if (!title || !description || !priority || !assignedTo) {
+        return res.status(400).json({ message: "Missing required fields." });
+        }
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Invalid project ID format." });
+        }
+        if (!mongoose.Types.ObjectId.isValid(assignedTo)) {
+            return res.status(400).json({ message: "Invalid user ID format." });
+        }
+
+        
+        const project = await ProjectModel.findById(id);
+        if (!project) return res.status(404).json({ message: "Project not found." });
+
+        const user = await UserModel.findById(assignedTo);
+        if (!user) return res.status(404).json({ message: "User not found." });
+
+        const task = await TaskModel.create({
+            title, 
+            description, 
+            priority,
+            project,
+            user
+        });
+        
+        return res.status(201).json({
+            success: true,
+            message:"Task created successfully.",
+            task: task
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success : false,
+            message: "Server internal error.",
             error: error.message
         });
     }
--- src/routes/Task.route.js
+++ src/routes/Task.route.js
@@ -1,10 +1,14 @@
 import e from "express";
-import { getListTask } from "../controllers/Task.controller.js";
+import { 
+    createTask, 
+    getListTask 
+} from "../controllers/Task.controller.js";
 const taskRoute = e.Router();
 
 
 
 taskRoute.get('/', getListTask);
+taskRoute.post('/:id/create', createTask);
 
 
 export default taskRoute;

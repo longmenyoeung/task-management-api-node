7dd8ccf8ac5d010777287292d725ccb43fa427a4
longmenyoeung <longmenyoeung@gmail.com>
2026-09-08 23:26:08 +0700

    deleteUser

--- src/routes/User.route.js
+++ src/routes/User.route.js
@@ -3,11 +3,13 @@ import {
     register,
     getList,
     searchById,
+    deleteUser,
 } from "../controllers/User.controller.js";
 const userRoute = express.Router();
 
 userRoute.get("/", getList);
 userRoute.get("/:id", searchById);
 userRoute.post("/register", register);
+userRoute.delete("/:id", deleteUser);
 
 export default userRoute;
